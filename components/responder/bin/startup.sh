c5045a8005fcef7049d5481111a0b6bbde2e18cd
drmcrooks <David.crooks@stfc.ac.uk>
2022-06-20 08:54:32 +0200

    Update startup.sh

--- components/responder/bin/startup.sh
+++ components/responder/bin/startup.sh
@@ -2,8 +2,8 @@
 
 cd /opt/pocketsoc-ng/bin/
 
-wget https://raw.githubusercontent.com/lukas2511/dehydrated/master/dehydrated
-chmod +x ./dehydrated
+curl -s https://raw.githubusercontent.com/lukas2511/dehydrated/master/dehydrated -O
+chmod +x /opt/pocketsoc-ng/bin/dehydrated
 
 python3 /opt/pocketsoc-ng/bin/alpn-responder.py
 
